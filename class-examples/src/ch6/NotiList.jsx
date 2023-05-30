8729f1dce2195bf7434caa234abc6026f42ba9ee
lsy3709 <lsy3709@naver.com>
2023-05-30 10:50:49 +0900

    230530 2교시 6장 실습 라이브코딩중

---
+++ class-examples/src/ch6/NotiList.jsx
@@ -0,0 +1,18 @@
+//NotiList.jsx 파일
+// 부모 컴포넌트
+
+//rcc 자동 클래스형 컴포넌트 생성.
+import React, { Component } from "react";
+import Noti from "./Noti";
+
+//임시 더미 데이터를 배열로 요소는 객체를 가지는 배열 만들기.
+
+const reserveNoti = [{}, {}, {}];
+
+class NotiList extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default NotiList;
