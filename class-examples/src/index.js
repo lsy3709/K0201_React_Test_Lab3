56252f87d94c97a3c1a9992dc205bfb027037993
lsy3709 <lsy3709@naver.com>
2023-05-31 11:30:51 +0900

    230531 3교시 7 hooks -  useMemo 예제 작업중2

--- class-examples/src/index.js
+++ class-examples/src/index.js
@@ -16,6 +16,7 @@ import NotiList from './ch6/NotiList';
 import MenuItemList from './ch6-2/MenuItemList';
 import TestSample from './ch6-3insert_delete/TestSample';
 import Counter from './ch7/Counter';
+import TestBefore from './ch7-2-useMemo/TestBefore';
 
 // 생명주기를 테스트를 하는 코드에서, React.StrictMode 제거하고 실행하는게 가독성면에서 좋습니다. 
 // 수행이 2번씩 일어남. 
@@ -26,7 +27,8 @@ ReactDOM.render(
     {/* <NotiList/> */}
     {/* <MenuItemList/> */}
     {/* <TestSample/> */}
-  <Counter/>
+  {/* <Counter/> */}
+  <TestBefore/>
   </React.StrictMode> ,
   document.getElementById('root')
 );
