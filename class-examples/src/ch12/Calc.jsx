f675e60d3983857e343cf92a3b0e0649d0cd7a8e
lsy3709 <lsy3709@naver.com>
2023-06-01 16:29:23 +0900

    230601 7교시 공유스테이트 코딩중. 및 실습

--- class-examples/src/ch12/Calc.jsx
+++ class-examples/src/ch12/Calc.jsx
@@ -9,18 +9,62 @@ const BoilMessage = (props) => {
   return <p>물이 끊지 않아요.</p>;
 };
 
+//변환해주는 함수 1
 //화씨 -> 섭씨
 const toCel = (fah) => {
   return ((fah - 32) * 5) / 9;
 };
-
+//변환해주는 함수 2
 //섭씨 -> 화씨
+const toFah = (cel) => {
+  return (cel * 9) / 5 + 32;
+};
+
+//컨버터 인자로 : 온도, 변환해주는 함수 1,2
+// 리턴: 섭씨 , 화씨 온도를 문자열로 받는다.
+const tryCon = (temp, convert) => {
+  // 텍스트 인풋 창, 숫자 입력이 아님.
+  const input = parseFloat(temp);
+  // 문자열 -> 실수(소수점표기)
+  // 체크 -> 숫자가 아닌 문자열이면, 공백으로 보내기.
+  if (Number.isNaN(input)) {
+    return "";
+  }
+
+  // convert : 위에서 정의한 , 각각 변환 해주는 함수 2가지.
+  const output = convert(input);
+  // 반올림 작업.
+  const rounded = Math.round(output * 1000) / 1000;
+  // 숫자 -> 문자열
+  return rounded.toString();
+};
 
 const Calc = () => {
+  const [temp, setTemp] = useState("");
+  const [scale, setScale] = useState("c");
+
+  const handleCelChange = (temp) => {
+    setTemp(temp);
+    setScale("c");
+  };
+
+  const handleFahChange = (temp) => {
+    setTemp(temp);
+    setScale("f");
+  };
+
+  const celsius = scale === "f" ? tryCon(temp, toCel) : temp;
+
+  const fah = scale === "c" ? tryCon(temp, toFah) : temp;
+
   return (
     <div>
-      <TestInput scale="c" />
-      <TestInput scale="f" />
+      {/* // 자식: props 목록 
+// 1) onTempChange 2) scale 3) temp */}
+      <TestInput scale="c" temp={celsius} onTempChange={handleCelChange} />
+      <TestInput scale="f" temp={fah} onTempChange={handleFahChange} />
+      {/* 100도 이상이면 메시지 표기  */}
+      <BoilMessage celsius={celsius} />
     </div>
   );
 };
--- class-examples/src/ch12/TestInput.jsx
+++ class-examples/src/ch12/TestInput.jsx
@@ -7,19 +7,24 @@ const sNames = {
 // props 로 전달해서 사용.
 // 공유 state 해서,
 // 부모 : Calc , ->(props) 자식 : TestInput
-const TestInput = (props) => {
-  const [temp, setTemp] = useState("");
 
+// 자식: props 목록
+// 1) onTempChange 2) scale 3) temp
+const TestInput = (props) => {
   const handleChange = (event) => {
     // 호출 되면서 -> 업데이트
-    setTemp(event.target.value);
+    // setTemp(event.target.value);
+    // 부모로부터 state 공유 받기.
+    props.onTempChange(event.target.value);
   };
 
   return (
     <div>
       <fieldset>
         <legend>온도를 입력하세요: (단위:{sNames[props.scale]})</legend>
-        <input value={temp} onChange={handleChange} />
+        {/* <input value={temp} onChange={handleChange} /> */}
+        {/* 공유 state로 온도를 사용하기. */}
+        <input value={props.temp} onChange={handleChange} />
         {/* 위에 끊는 부분에 알림 메세지 컴포넌트에 해당 props 로 celsius 값을 전달은
             온도 타입 문자열 -> 실수 변경. */}
       </fieldset>
